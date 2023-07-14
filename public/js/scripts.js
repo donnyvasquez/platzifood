1cb89b275e00f29643ea8137bf835ebdb1782a73
DONNY <dvasquez@donnyvasquez.com>
2023-07-14 15:55:18 -0500

    first commit

---
+++ public/js/scripts.js
@@ -0,0 +1,5 @@
+menuButton.addEventListener('click', function () {
+  let classList = document.getElementById('nav').classList
+  classList.toggle('hidden')
+  classList.toggle('block')
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+Comandos para desarrollo
+npx tailwindcss -i ./css/tailwind.css -o ./public/css/style.css --watch 
+npm run build --watch  
+live-server public
